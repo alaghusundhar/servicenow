244a2d990e2721f91675a0e8624a2514f86835b2
Nithyanandam <alaghusundhar@hotmail.com>
2022-11-02 12:30:47 -0500

    added file for servicenow automation

---
+++ createChangeRequestPipeline.groovy
@@ -0,0 +1,4 @@
+@Library(['shared-lib']) _
+
+createChangeRequestPostDeployment {
+}
---
+++ vars/createChangeRequestPostDeployment.groovy
@@ -0,0 +1,54 @@
+// Author:- alagusundaram.nithyanandam@sap.com
+import groovy.json.JsonSlurperClassic
+
+void call(body) {
+
+ String strDataCenter = ''
+ String strEnvironmentName = ''
+ String strServiceName = ''
+ String strReleaseTagName = ''   
+}
+
+pipeline {
+    agent {
+        stages {
+            stage('Set Parameters') {
+                steps {
+                    script {
+                        strDataCenter= "$DATACENTER"
+                        strEnvironmentName ="$ENVIRONMENT"
+                        strServiceName= "$SVCNAME"
+                        strReleaseTagName ="$IMAGE_VERSION"
+                    }
+                }
+            }
+            stage('Validate Parameters') {
+                steps {
+                    script {
+                        if (!strDataCenter){
+                            error('Datacenter must be set . !!!')
+                        }
+                        if (!strEnvironmentName) {
+                            error('EnvirnmentName must be set . !!!')
+                        }
+                        if (!strServiceName) {
+                            error('Service name must be set . !!!')
+                        }
+                        if (!strReleaseTagName) {
+                            error('Release Tag must be set . !!!')
+                        }
+                    }
+                }
+            }
+            stage('Create Standard Change Request') {
+                steps {
+                    script {
+
+                    }
+                }
+            }
+        }
+
+    }
+
+}
