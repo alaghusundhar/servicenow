42cb14f55aa7e1d13aeeaacf1327e88117e159ab
Nithyanandam <alaghusundhar@hotmail.com>
2022-11-02 15:13:58 -0500

    changing code structure

--- vars/createChangeRequestPostDeployment.groovy
+++ vars/createChangeRequestPostDeployment.groovy
@@ -12,42 +12,41 @@ void call(body) {
 
 pipeline {
     agent none
-        stages {
-            stage('Set Parameters') {
-                steps {
-                    script {
-                        strDataCenter= "$DATACENTER"
-                        strEnvironmentName ="$ENVIRONMENT"
-                        strServiceName= "$SVCNAME"
-                        strReleaseTagName ="$IMAGE_VERSION"
-                    }
+    stages {
+        stage('Set Parameters') {
+            steps {
+                script {
+                    strDataCenter= "$DATACENTER"
+                    strEnvironmentName ="$ENVIRONMENT"
+                    strServiceName= "$SVCNAME"
+                    strReleaseTagName ="$IMAGE_VERSION"
                 }
             }
-            stage('Validate Parameters') {
-                steps {
-                    script {
-                        if (!strDataCenter){
-                            error('Datacenter must be set . !!!')
-                        }
-                        if (!strEnvironmentName) {
-                            error('EnvirnmentName must be set . !!!')
-                        }
-                        if (!strServiceName) {
-                            error('Service name must be set . !!!')
-                        }
-                        if (!strReleaseTagName) {
-                            error('Release Tag must be set . !!!')
-                        }
+        }
+        stage('Validate Parameters') {
+            steps {
+                script {
+                    if (!strDataCenter){
+                        error('Datacenter must be set . !!!')
                     }
-                }
-            }
-            stage('Create Standard Change Request') {
-                steps {
-                    script {
-
+                    if (!strEnvironmentName) {
+                        error('EnvirnmentName must be set . !!!')
+                    }
+                    if (!strServiceName) {
+                        error('Service name must be set . !!!')
+                    }
+                    if (!strReleaseTagName) {
+                        error('Release Tag must be set . !!!')
                     }
                 }
             }
         }
+        stage('Create Standard Change Request') {
+            steps {
+                script {
 
+                }
+            }
+        }
+    }
 }
